3f02038e1d41abcc1cea510492cab34ed575ed1f
jfranchi <jefferson.fantasia@gmail.com>
2021-07-30 17:17:32 -0300

    create or upload files

--- get_next_line.c
+++ get_next_line.c
@@ -6,92 +6,95 @@
 /*   By: jfranchi <jfranchi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/20 19:41:46 by jfranchi          #+#    #+#             */
-/*   Updated: 2021/07/30 16:49:05 by jfranchi         ###   ########.fr       */
+/*   Updated: 2021/07/30 17:17:05 by jfranchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char    *before_line_breaker(char *save)
+char	*before_line_breaker(char *save)
 {
-    char    *current_line;
-    int        i;
+	char	*current_line;
+	int		i;
 
-    if (!save)
-        return (NULL);
-    i = 0;
-    while (save[i] && save[i] != '\n')
-        i++;
-    current_line = (char *)ft_calloc((i + 1), sizeof(char));
-    if (!current_line)
-        return (NULL);
-    i = 0;
-    while (save[i] && save[i] != '\n')
-    {
-        current_line[i] = save[i];
-        i++;
-    }
-    return (current_line);
+	if (!save)
+		return (NULL);
+	i = 0;
+	while (save[i] && save[i] != '\n')
+		i++;
+	current_line = (char *)ft_calloc((i + 1), sizeof(char));
+	if (!current_line)
+		return (NULL);
+	i = 0;
+	while (save[i] && save[i] != '\n')
+	{
+		current_line[i] = save[i];
+		i++;
+	}
+	return (current_line);
 }
 
-char    *after_line_breaker(char *save)
+char	*after_line_breaker(char *save)
 {
-    char    *next_line;
-    int        i;
-    int        j;
+	char	*next_line;
+	int		i;
+	int		j;
 
-    if (!save)
-        return (NULL);
-    i = 0;
-    while (save[i] && save[i] != '\n')
-        i++;
-    if (!save[i])
-    {
-        free(save);
-        return (0);
-    }
-    next_line = (char *)ft_calloc((ft_strlen(save) - i), sizeof(char));
-    if (!next_line)
-        return (NULL);
-    i++;
-    j = 0;
+	if (!save)
+		return (NULL);
+	i = 0;
+	while (save[i] && save[i] != '\n')
+		i++;
+	if (!save[i])
+	{
+		free(save);
+		return (NULL);
+	}
+	next_line = (char *)ft_calloc((ft_strlen(save) - i), sizeof(char));
+	if (!next_line)
+		return (NULL);
+	i++;
+	j = 0;
 	while (save[i] != '\0')
 		next_line[j++] = save[i++];
 	free(save);
-    if (ft_strcmp(next_line, ""))
+	if (ft_strcmp(next_line, ""))
+	{
+		free(next_line);
 		return (NULL);
-    return (next_line);
+	}
+	return (next_line);
 }
 
-char    *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    char        *current_line;
-    char        *buffer;
-    static char    *save;
-    ssize_t        ret;
+	char		*current_line;
+	char		*buffer;
+	static char	*save;
+	ssize_t		ret;
 
-    buffer = (char *)ft_calloc((BUFFER_SIZE + 1), sizeof(char));
-    if (!buffer)
-        return (NULL);
-    ret = 1;
-    while (ret != 0 && !ft_strchr(save, '\n'))
-    {
-        ret = read(fd, buffer, BUFFER_SIZE);
-        if (ret == -1)
-        {
-            free(buffer);
-            return (NULL);
-        }
-        buffer[ret] = '\0';
-        save = ft_strjoin(save, buffer);
-    }
-    free(buffer);
-    current_line = before_line_breaker(save);
-    save = after_line_breaker(save);
-    if (ret == 0 && save == NULL)
-    {
-        free(current_line);
-        return (NULL);
-    }
-    return (current_line);
+	buffer = (char *)ft_calloc((BUFFER_SIZE + 1), sizeof(char));
+	if (!buffer)
+		return (NULL);
+	ret = 1;
+	while (ret != 0 && !ft_strchr(save, '\n'))
+	{
+		ret = read(fd, buffer, BUFFER_SIZE);
+		if (ret == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[ret] = '\0';
+		save = ft_strjoin(save, buffer);
+	}
+	free(buffer);
+	current_line = before_line_breaker(save);
+	save = after_line_breaker(save);
+	if (ret == 0 && save == NULL)
+	{
+		free(current_line);
+		return (NULL);
+	}
+	return (current_line);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jfranchi <jfranchi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/21 19:09:43 by jfranchi          #+#    #+#             */
-/*   Updated: 2021/07/30 16:10:55 by jfranchi         ###   ########.fr       */
+/*   Updated: 2021/07/30 17:14:30 by jfranchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,27 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-int    main(int argc, char const *argv[])
+int		main(int argc, char const *argv[])
 {
-    ssize_t    fd;
-    char *test;
-    fd = 0;
-    if (argc == 1)
-        fd = 0;
-    else if (argc == 2)
-        fd = open(argv[1], O_RDONLY);
-    else
-        return (2);
-    do
-    {
-        test = get_next_line(fd);
-        if (test)
-            free(test);
-    }
-    while (test != NULL);
-    if (test)
-        free(test);
-    close (fd);
-    return (0);
+	ssize_t    fd;
+	char *test;
+	fd = 0;
+	if (argc == 1)
+		fd = 0;
+	else if (argc == 2)
+		fd = open(argv[1], O_RDONLY);
+	else
+		return (2);
+	do
+	{
+		test = get_next_line(fd);
+		printf("|%s|\n", test);
+		if (test)
+			free(test);
+	}
+	while (test != NULL);
+	if (test)
+		free(test);
+	close (fd);
+	return (0);
 }
